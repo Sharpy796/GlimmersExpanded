297a2cf46cd8ef9eb97f56bcf0d13bf1f56b18f3
Sharpy796 <christian.bankovic@gmail.com>
2024-08-31 17:30:38 -0500

    Made gun_actions.lua a lot faster and simpler

--- files/addGlimmers.lua
+++ files/addGlimmers.lua
@@ -1,16 +1,6 @@
 dofile("mods/GlimmersExpanded/files/lib/glimmer_data.lua")
 local mod_id = "GLIMMERS_EXPANDED_"
 glimmer_list_revamped = {}
-organizedGlimmerList = {
-	{id="COLOUR_RED",sort_after=1},
-	{id="COLOUR_ORANGE",sort_after=2},
-	{id="COLOUR_YELLOW",sort_after=3},
-	{id="COLOUR_GREEN",sort_after=4},
-	{id="COLOUR_BLUE",sort_after=5},
-	{id="COLOUR_PURPLE",sort_after=6},
-	{id="COLOUR_RAINBOW",sort_after=7},
-	{id="COLOUR_INVIS",sort_after=8},
-}
 
 -- Adds a new glimmer to the game.
 ---@param name string
@@ -59,36 +49,6 @@ function addGlimmer(name, desc, materials, image, cast_delay, spawn_tiers, sort_
     -- print("Adding '"..newGlimmer.name.."' with material '"..newGlimmer.materials[1].."' to glimmer_list_revamped")
     glimmer_list_revamped[id] = newGlimmer
 end
-
--- addGlimmer("White", "Gives a projectile a white sparkly trail", {"spark_white"}, "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_white.png", nil, "2,3,4", -100, nil, false)
--- addGlimmer("Pink", "Gives a projectile a pink sparkly trail", {"plasma_fading_pink"}, "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_pink.png", nil, "1,2,3", 0, nil, false)
--- addGlimmer("Weird Fungus", "Gives a projectile a trippy sparkly trail", {"fungi","blood_fungi",}, "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_weird_fungus.png", nil, "2,3,4,5", 0.1, nil, false)
--- -- COLOUR_RED - sort_after = 1
--- addGlimmer("Blood", "Gives a projectile a bloody sparkly trail", {"blood","blood_fading","blood_fading_slow","cloud_blood",}, "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_blood.png", nil, "1,2,3,4,5,6", 1.1, nil, false)
--- -- COLOUR_ORANGE - sort_after = 2
--- addGlimmer("Lava", "Gives a projectile a molten sparkly trail", {"lava"}, "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_lava.png", 15, "2,3,4,5,6", 2.1, nil, false)
--- -- COLOUR_YELLOW - sort_after = 3
--- -- COLOUR_GREEN - sort_after = 4
--- addGlimmer("Acid", "Gives a projectile a caustic sparkly trail", {"acid"}, "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_acid.png", 15, "2,3,4,5,6", 4.1, nil, false)
--- addGlimmer("Diminution", "Gives a projectile a weakened sparkly trail", {"magic_liquid_weakness"}, "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_diminution.png", 15, "2,3,4,5", 4.2, nil, false)
--- addGlimmer("Teal", "Gives a projectile a teal sparkly trail", {"spark_teal"}, "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_teal.png", nil, "1,2,3,4", 4.3, nil, false)
--- -- COLOUR_BLUE - sort_after = 5
--- addGlimmer("Freezing Liquid", "Gives a projectile a frozen sparkly trail", {"blood_cold"}, "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_freezing_liquid.png", 15, "2,3,4,5", 5.1, nil, false)
--- -- COLOUR_PURPLE - sort_after = 6
--- addGlimmer("Ominous", "Gives a projectile an ominous sparkly trail", {"material_darkness"}, "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_ominous.png", 15, "3,4,5", 6.1, nil, false)
--- addGlimmer("Void", "Gives a projectile a trail darker than the void itself", {"void_liquid"}, "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_void.png", nil, "3,4,5,6", 6.2, nil, false)
--- addGlimmer("Mimicium", "Gives a projectile an iridescent sparkly trail", {"mimic_liquid"}, "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_mimicium.png", nil, "4,5,6,10", 6.3, nil, false)
--- -- COLOUR_RAINBOW - sort_after = 7
--- addGlimmer("True Rainbow", "Gives a projectile a truly rainbow sparkly trail", {"material_rainbow"}, "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_true_rainbow.png", nil, "4,5,6,10", 7.1, nil, false)
--- addGlimmer("Midas", "Gives a projectile a wealthy sparkly trail", {"midas"}, "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_midas.png", 4, "4,5,6,10", 7.2, nil, true)
--- addGlimmer("Lively Concoction", "Gives a projectile a regenerative sparkly trail", {"magic_liquid_hp_regeneration_unstable"}, "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_lively_concoction.png", 4, "4,5,6,10", 7.3, nil, true)
--- addGlimmer("Divine Ground", "Gives a projectile a holy sparkly trail", {"grass_holy"}, "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_divine_ground.png", 4, "4,5,6,10", 7.31, nil, true)
--- addGlimmer("Biome", "Gives a projectile a sparkly trail that changes depending on the biome you are in!", {"sand","sand_surface","sandstone","sandstone_surface","snow","snow_sticky","slush","water","water_ice","water_salt","water_fading","water_temp","water","water_swamp","grass",}, "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_biome.png", nil, "4,5,6,10", 7.4, nil, false)
--- -- COLOUR_INVIS - sort_after = 8
-
--- Example Line:
--- addGlimmer("Vomit", "Gives a projectile a sickeningly sparkly trail", {"vomit"}, nil, 15, "1,2", 4.21, "EXAMPLE")
--- function addGlimmer(name, desc, materials, image, cast_delay, spawn_tiers, sort_after, mod_prefix)
 
 for _,glimmer in ipairs(glimmer_data) do
     addGlimmer(glimmer.name,
--- files/scripts/gun/gun_actions.lua
+++ files/scripts/gun/gun_actions.lua
@@ -2,14 +2,14 @@ dofile_once("mods/GlimmersExpanded/files/addGlimmers.lua")
 local Mod_Id = "GLIMMERS_EXPANDED_COLOUR_"
 
 local originalGlimmers = {
-	["COLOUR_RED"]=true,
-	["COLOUR_ORANGE"]=true,
-	["COLOUR_YELLOW"]=true,
-	["COLOUR_GREEN"]=true,
-	["COLOUR_BLUE"]=true,
-	["COLOUR_PURPLE"]=true,
-	["COLOUR_RAINBOW"]=true,
-	["COLOUR_INVIS"]=true,
+	["COLOUR_RED"]=1,
+	["COLOUR_ORANGE"]=2,
+	["COLOUR_YELLOW"]=3,
+	["COLOUR_GREEN"]=4,
+	["COLOUR_BLUE"]=5,
+	["COLOUR_PURPLE"]=6,
+	["COLOUR_RAINBOW"]=7,
+	["COLOUR_INVIS"]=8,
 }
 
 local function split(string)
@@ -32,12 +32,9 @@ local total_spawns = {
 	["10"]={prob=0.3,amt=2},
 }
 
-local myFancyNewColors =
-{
+local myFancyNewColors = {}
 
-}
-
-local function createGlimmerAction(Id, image, wait_frames, spawn_probs, spawn_tiers, unlock_flag, custom_action)
+local function createGlimmerAction(Id, image, wait_frames, spawn_probs, spawn_tiers, sort_after, unlock_flag, custom_action)
 	local MOD_ID = Mod_Id:upper()
 	local mod_id = Mod_Id:lower()
 	local ID = Id:upper()
@@ -46,6 +43,7 @@ local function createGlimmerAction(Id, image, wait_frames, spawn_probs, spawn_ti
 	if image == nil then image = "mods/GlimmersExpanded/files/gfx/ui_gfx/colour_unknown.png" end
     if spawn_probs == nil then spawn_probs = "0.2,0.2,0.2,0.2,0.2,0.2" end
 	if spawn_tiers == nil then spawn_tiers = "1,2,3,4,5,6" end
+    if sort_after == nil then sort_after = 100 end
 	if unlock_flag == nil then unlock_flag = "card_unlocked_paint" end
     if type(custom_action) ~= "function" then custom_action = function(c) --[[Do nothing]] end end
 
@@ -71,20 +69,14 @@ local function createGlimmerAction(Id, image, wait_frames, spawn_probs, spawn_ti
         price                   = 40,
         mana                    = 0,
         action 					= action,
+        sort_after              = sort_after,
     }
     table.insert(myFancyNewColors, newGlimmer)
 	return newGlimmer
 end
 
-local function insertIntoProgress(id, data)
-	local sort_after = data.sort_after
-	local entry = {id=id,sort_after=sort_after}
-	-- print("Inserting '"..id.."' into '"..sort_after.."' in progress")
-	table.insert(organizedGlimmerList, entry)
-end
-
-local function sortProgress()
-	table.sort(organizedGlimmerList, sortGlimmers)
+local function sortProgress(myTable)
+	table.sort(myTable, sortGlimmers)
 end
 
 function sortGlimmers(entry1, entry2)
@@ -92,20 +84,15 @@ function sortGlimmers(entry1, entry2)
 end
 
 for id, data in pairs(glimmer_list_revamped) do
-	insertIntoProgress(id, data)
-	createGlimmerAction(id, data.image, data.cast_delay, nil, data.spawn_tiers, nil, data.custom_action)
+	createGlimmerAction(id, data.image, data.cast_delay, nil, data.spawn_tiers, data.sort_after, nil, data.custom_action)
 end
-
-sortProgress()
 
 
 local function parseSpawnProbs(action)
-    -- print("----------- "..action.id.." -----------")
 	-- replace spawn probs with averaged ones
 	local spawn_levels = split(action.spawn_level)
 	local new_probabilities = ""
 	for index,level in ipairs(spawn_levels) do
-        -- print("'"..action.id.."', tier '"..level.."', probability '"..total_spawns[level].prob.."', amount '"..total_spawns[level].amt.."'")
 		new_probabilities = new_probabilities..total_spawns[level].prob
 		if index < #spawn_levels then
 			new_probabilities = new_probabilities .. ","
@@ -127,14 +114,12 @@ for level,data in pairs(total_spawns) do
 	total_spawns[level].prob = total_spawns[level].prob/(total_spawns[level].amt)
 end
 
-
+-- Fix spawn rates for modded glimmers
 for _, color in ipairs(myFancyNewColors) do
 	color.spawn_probability = parseSpawnProbs(color)
-
-	-- print("inserting spell '"..color.id.."' into actions")
-	table.insert(actions,color)
 end
 
+-- Fix spawn rates for vanilla glimmers
 for _, action in ipairs(actions) do
     if originalGlimmers[action.id] then
         action.spawn_probability = parseSpawnProbs(action)
@@ -142,48 +127,44 @@ for _, action in ipairs(actions) do
 end
 
 if ModSettingGet("GlimmersExpanded.inject_spells") then
+    -- Remove the original glimmers from actions, and insert them into myFancyNewColors
+    for index, action in ipairs(actions) do
+        if index >= #actions then break end
+        local id = actions[index].id
+        local isGlimmer = originalGlimmers[id]
+        if isGlimmer then
+            repeat
+                local id = actions[index].id
+                isGlimmer = originalGlimmers[id]
+                if isGlimmer then
+                    actions[index].sort_after = isGlimmer
+                    table.insert(myFancyNewColors, table.remove(actions, index))
+                end
+            until (not isGlimmer) or index > #actions
+        end
+    end
+end
 
-    -- Get a list of all glimmers
-	local allGlimmerList = {}
-	for _, data in ipairs(organizedGlimmerList) do
-		allGlimmerList[data.id] = data.id
-	end
-    -- print("Finished gathering a list of all glimmers")
+-- Sort myFancyNewColors
+sortProgress(myFancyNewColors)
 
-    -- Remove all glimmers from progress
-	for pos, action in ipairs(actions) do
-		if pos >= #actions then break end
-		local id = action.id
-		local isGlimmer = allGlimmerList[id]
-		if isGlimmer then
-			repeat
-				id = actions[pos].id
-				isGlimmer = allGlimmerList[id]
-				if isGlimmer then
-					allGlimmerList[isGlimmer] = table.remove(actions, pos)
-				end
-			until (not isGlimmer) or pos > #actions
-		end
-	end
-	-- print("Finished populating allGlimmerList and removing spells from actions")
-
-    -- Put them back into progress
-	for pos, action in ipairs(actions) do
-		if pos > #actions then
-			for _, data in ipairs(organizedGlimmerList) do
-                -- print("inserting '"..allGlimmerList[data.id].id.."' into actions")
-				table.insert(actions, allGlimmerList[data.id])
-			end
-		elseif action.id == "IF_ELSE" then
-			for _, data in ipairs(organizedGlimmerList) do
-				if data then
-					pos = pos + 1
-                    -- print("inserting '"..allGlimmerList[data.id].id.."' into actions at position "..pos)
-					table.insert(actions, pos, allGlimmerList[data.id])
-				end
-			end
-			break
-		end
-	end
-	-- print("Finished inserting spells back into actions")
+-- Put myFancyNewColors into actions
+if ModSettingGet("GlimmersExpanded.inject_spells") then
+    for index, action in ipairs(actions) do
+        if index > #actions then
+            for _, color in ipairs(myFancyNewColors) do
+                table.insert(actions, color)
+            end
+        elseif action.id == "IF_ELSE" then
+            for _, color in ipairs(myFancyNewColors) do
+                index = index + 1
+                table.insert(actions, index, color)
+            end
+            break
+        end
+    end
+else
+    for _, color in ipairs(myFancyNewColors) do
+        table.insert(actions, color)
+    end
 end
