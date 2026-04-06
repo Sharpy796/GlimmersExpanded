ed4beeb85facf6f46712491a188b09040f5a9994
Christian <christian.bankovic@gmail.com>
2026-04-06 10:30:11 -0500

    cleanup

--- data/scripts/projectiles/colour_spell_COPYPASTE.lua
+++ data/scripts/projectiles/colour_spell_COPYPASTE.lua
@@ -355,7 +355,6 @@ if ( colour ~= nil ) then
 	if ( comps ~= nil ) then
 		if particle ~= nil then
 			for i,v in ipairs( comps ) do
-				-- local hex,r,g,b,a = material_to_rgba(particle)
 				ComponentSetValue2(v, "color", r,g,b,a)
 				ComponentSetValue2( v, "is_emitting", true )
 			end
@@ -420,7 +419,6 @@ if ( colour ~= nil ) then
 			if (mixing and i == #comps) or (not mixing) or (colour == "invis") then
 				if ( particle ~= nil ) then
 					local spritefilepath, additive = create_vsc(entity_id, v, i, "explosion_sprite", "explosion_sprite_additive", "explosionspriteoriginal", "config_explosion")
-					-- local hex,r,g,b,a = material_to_rgba(particle)
 					set_additive(r,g,b, v, "explosion_sprite_additive", additive, "config_explosion")
 
 					if spritefilepath ~= nil and spritefilepath ~= "" then
@@ -439,26 +437,4 @@ if ( colour ~= nil ) then
 			end
 		end
 	end
-
-	-- comps = EntityGetComponent( entity_id, "LightComponent" )
-	-- if ( comps ~= nil ) then
-	-- 	for i,v in ipairs( comps ) do
-	-- 		if colour == "invis" then
-	-- 			-- TODO: How do I make lights turn off??
-	-- 		else
-	-- 			if ( particle ~= nil ) then
-	-- 				ComponentSetValue2(v, "update_properties", true)
-	-- 				if mixing then
-	-- 					ComponentSetValue2(v,"r",(r+ComponentGetValue2(v,"r"))/2)
-	-- 					ComponentSetValue2(v,"g",(g+ComponentGetValue2(v,"g"))/2)
-	-- 					ComponentSetValue2(v,"b",(b+ComponentGetValue2(v,"b"))/2)
-	-- 				else
-	-- 					ComponentSetValue2(v,"r",r)
-	-- 					ComponentSetValue2(v,"g",g)
-	-- 					ComponentSetValue2(v,"b",b)
-	-- 				end
-	-- 			end
-	-- 		end
-	-- 	end
-	-- end
 end
